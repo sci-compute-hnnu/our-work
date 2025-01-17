85fcc2b1135c675df53f71f8cb44f974173db022
ChangFengya <2059565724@qq.com>
2025-01-17 14:55:34 +0800

    GUI/one_layer/Mesh/_3D_Mesh/main.py

--- src/Pre/MeshGeneration.py
+++ src/Pre/MeshGeneration.py
@@ -1,3 +1,4 @@
+import gmsh
 import pygmsh
 
 
@@ -17,4 +18,119 @@ def _2D_generate_mesh(shape, mesh_size, algorithm):
         mesh = geom.generate_mesh(int(algorithm))
 
     return mesh
+
+
+def _3D_generate_mesh(shape, mesh_size, algorithm, mesh_type):
+    gmsh.initialize()
+    gmsh.model.add("model")
+    if shape[0] == "box":
+        gmsh.model.occ.addBox(shape[1], shape[2], shape[3], shape[4], shape[5], shape[6])
+    elif shape[0] == "sphere":
+        gmsh.model.occ.addSphere(shape[1], shape[2], shape[3], shape[4])
+    elif shape[0] == "other":
+        # 定义点和面
+        point = shape[1]
+        face = shape[2]
+
+        # 添加点
+        points = [gmsh.model.occ.addPoint(*pt, mesh_size) for pt in point]
+
+        # 创建一个存储线段的字典
+        lines_dict = {}
+        lines = []
+
+        # 添加线段
+        def add_line(p1, p2):
+            if (p1, p2) not in lines_dict and (p2, p1) not in lines_dict:
+                line_id = gmsh.model.occ.addLine(p1, p2)
+                lines_dict[(p1, p2)] = line_id
+                lines_dict[(p2, p1)] = line_id
+                return line_id
+            return lines_dict.get((p1, p2)) or lines_dict.get((p2, p1))
+
+        for f in face:
+            for i in range(len(f)):
+                p1 = f[i]
+                p2 = f[(i + 1) % len(f)]
+                add_line(p1, p2)
+
+        # 根据存储的线段定义线环
+        def get_curve_loop(vertex_ids):
+            loop_lines = []
+            for i in range(len(vertex_ids)):
+                p1 = vertex_ids[i]
+                p2 = vertex_ids[(i + 1) % len(vertex_ids)]
+                line_id = lines_dict.get((p1, p2)) or lines_dict.get((p2, p1))
+                if line_id is not None:
+                    loop_lines.append(line_id)
+                else:
+                    raise ValueError(f"Line segment ({p1}, {p2}) not found in lines_dict.")
+            return gmsh.model.occ.addCurveLoop(loop_lines)
+
+        # 生成面环（face loops）
+        def generate_face_loops(faces):
+            face_loops = []
+            for face in faces:
+                face_loop = get_curve_loop(face)
+                face_loops.append(face_loop)
+            return face_loops
+
+        # 计算面环
+        face_loops = generate_face_loops(face)
+
+        # 添加面
+        faces = [gmsh.model.occ.addPlaneSurface([loop]) for loop in face_loops]
+
+        # 创建体
+        surface_loop = gmsh.model.occ.addSurfaceLoop(faces)
+        gmsh.model.occ.addVolume([surface_loop])
+
+    # 同步几何模型
+    gmsh.model.occ.synchronize()
+    gmsh.model.mesh.setSize(gmsh.model.getEntities(0), mesh_size)
+    gmsh.option.setNumber("Mesh.Algorithm3D", int(algorithm))
+
+    gmsh.model.mesh.generate(int(mesh_type))
+    node_tags, node_coords, _ = gmsh.model.mesh.getNodes()
+    element_tags, element_types, element_nodes = gmsh.model.mesh.getElements(-1, -1)
+
+    element_type_names = {
+        1: "line",
+        2: "triangle",
+        3: "quadrilateral",
+        4: "tetrahedron",
+        5: "hexahedron",
+        6: "wedge",
+        7: "pyramid",
+        8: "quadratic line",
+        9: "quadratic triangle",
+        10: "quadratic quadrilateral",
+        11: "cubic tetrahedron",
+        12: "cubic hexahedron",
+        13: "cubic wedge",
+        14: "cubic pyramid",
+        15: "linear triangle",
+        16: "linear quadrilateral",
+        17: "linear tetrahedron",
+        18: "linear hexahedron",
+        # 可以根据需要添加更多类型
+    }
+    # 获取单元数据
+    cells = {}
+    # 遍历每种元素类型
+    for i, tag in enumerate(element_tags):
+        etype = element_tags[i]  # 获取单元类型 ID
+        type_name = element_type_names.get(etype, "unknown")  # 获取类型名称
+        element_array = element_types[i]  # 获取对应的 element_type 数组
+
+        # 将类型名称和对应的 element_type 数组添加到 cells 字典
+        if type_name not in cells:
+            cells[type_name] = []
+        cells[type_name].extend(element_array.tolist())  # 将数组转换为列表并添加
+
+    gmsh.finalize()
+
+    return node_coords, element_nodes, element_tags, cells
+
+
 
