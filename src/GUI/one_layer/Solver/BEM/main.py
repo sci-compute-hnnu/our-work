d9f6acd8e83a3c5b0e949f26ed3a177fb9bd6a82
synue22 <1303736297@qq.com>
2025-02-15 00:53:16 +0800

    新增两个按键、截图功能和BEM初值选项

--- src/GUI/one_layer/Solver/BEM/main.py
+++ src/GUI/one_layer/Solver/BEM/main.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 from Solve.BEM._3D_const_BEM_Laplace import BEM_3DCESolver
 from GUI.one_layer.Solver.SolverWindow import SolverWindow
 
@@ -7,6 +9,22 @@ from GUI.one_layer.Solver.SolverWindow import SolverWindow
 class BEMSolverWindow(SolverWindow):
     def __init__(self, box1, notebook, two_layer, three_layer):
         super().__init__('../../ui/solver.glade', box1, notebook, two_layer, three_layer)
+
+        # 选择框
+        self.combobox_BEM = self.builder.get_object("combobox_BEM")
+        self.combobox_BEM.set_sensitive(True)
+        # 确保是 ComboBoxText
+        if isinstance(self.combobox_BEM, Gtk.ComboBoxText):
+            options = [
+                "1/np.sqrt((x[:,0]-1)**2+(x[:,1]-1)**2+(x[:,2]-1)**2)",
+                "1/np.sqrt(x[:,0]**2 + x[:,1]**2 + x[:,2]**2)"
+            ]
+
+            for option in options:
+                self.combobox_BEM.append_text(option)
+
+        # 默认不选中任何一个
+        self.combobox_BEM.set_active(-1)
 
         # 边值条件输入框
         self.bc_entry = self.builder.get_object("entry")
